0bde2d97deb5c3322c4dd385fb83e5faa3c39af3
manasvinir <manasvini.raghunathan@gmail.com>
2025-10-03 14:31:44 -0500

    initialization of project

---
+++ src/main/java/com/manasvinir/AirplaneOperationsPortal/AirplaneOperationsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.manasvinir.AirplaneOperationsPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneOperationsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneOperationsPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manasvinir/AirplaneOperationsPortal/AirplaneOperationsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manasvinir.AirplaneOperationsPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneOperationsPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
