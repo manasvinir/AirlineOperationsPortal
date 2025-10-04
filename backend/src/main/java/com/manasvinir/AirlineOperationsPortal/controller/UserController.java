7008f1a9877b007b4b043cd94daf294ecf64d8ff
manasvinir <manasvini.raghunathan@gmail.com>
2025-10-04 16:57:56 -0500

    fixed all runtime errors

--- backend/src/main/java/com/manasvinir/AirlineOperationsPortal/controller/UserController.java
+++ backend/src/main/java/com/manasvinir/AirlineOperationsPortal/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.manasvinir.AirlineOperationsPortal.controller;
 
+import com.manasvinir.AirlineOperationsPortal.entity.User;
+import com.manasvinir.AirlineOperationsPortal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")     //standard url for initial release
